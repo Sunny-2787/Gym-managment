fb4fb29c8703415262ab369bf0995b10540c4632
Sunny <cse_0182320012101265@lus.ac.bd>
2025-08-23 01:17:06 +0600

    Redy for Deploy

--- gym/settings.py
+++ gym/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-xt)3(!qylgpond-i*p^!d@j99zo46@j_y-!q#muhcjfrz*iwx-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com' ,'http://127.0.0.1:8000']
 
 
 # Application definition
